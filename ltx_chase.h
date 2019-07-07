e48b855895c4795b9beb38b4a4f8dda6c9c86970
Davide -limaCAT- Inglima <limacat@live.it>
2019-07-07 16:28:39 +0200

    Removed three definitions in ltx_chase.h

--- ltx_chase.h
+++ ltx_chase.h
@@ -5,9 +5,4 @@
 extern cvar_t chase_active;
 extern cvar_t cl_viewmodel_scale;
 
-// TODO limaCAT maybe it does not exist?
-void Chase_Init(void);
-void Chase_Reset(void);
-void Chase_Update(void);
-
 #endif
