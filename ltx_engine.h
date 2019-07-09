ff2c52018863a44aa774e94e6bf8d7e6622b6e55
Davide -limaCAT- Inglima <limacat@live.it>
2019-07-09 14:21:45 +0200

    Tracking the authors of the various son-of-quakedef files.

--- ltx_engine.h
+++ ltx_engine.h
@@ -1,3 +1,30 @@
+/*
+Copyright (C) 1996-1997 Id Software, Inc.
+Copyright (C) 2003 Forest Hale
+Copyright (C) 2019 Davide Inglima
+Copyright (C) black, divverent, eihrul, knghtbrd, havoc, molivier, Nikoli, res, vortex
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+*/
+/*
+Quake is a trademark of Id Software, Inc., (c) 1996 Id Software, Inc. All
+rights reserved.
+*/
+
 #ifndef LTX_ENGINE_H
 #define LTX_ENGINE_H
 
--- quakedef.h
+++ quakedef.h
@@ -1,5 +1,8 @@
 /*
 Copyright (C) 1996-1997 Id Software, Inc.
+Copyright (C) 2003 Forest Hale
+Copyright (C) 2019 Davide Inglima
+Copyright (C) black, divverent, eihrul, knghtbrd, havoc, molivier, Nikoli, res, vortex
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
@@ -17,7 +20,10 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 */
-// quakedef.h -- primary header for client
+/* 
+Quake is a trademark of Id Software, Inc., (c) 1996 Id Software, Inc. All
+rights reserved.
+*/
 
 #ifndef QUAKEDEF_H
 #define QUAKEDEF_H
